de050470fefddc2e02a0d1bbb3dfc8e11473fa2b
gremixas <andrius.agafonovas@gmail.com>
2023-01-16 20:44:23 +0200

    Pets progress......

--- PHP/Pets/pet_functions.php
+++ PHP/Pets/pet_functions.php
@@ -6,20 +6,19 @@ function getPets(string $filePath): array {
     return json_decode(file_get_contents($filePath), true) ?? [];
 }
 
-function getPet($arr, $id): array {
+function getPet(array $arr, $id): array {
     return array_values(array_filter($arr, fn($pet) => $pet['id'] == $id))[0];
 }
 
-function createRecord(string $filePath, array $pet_info): int {
+function createRecord(string $filePath, array $petInfo): int {
     $pets = getPets($filePath);
-    $allIds = array_column($pets, "id") ?? [1];
+    $allIds = array_column($pets, "id");
     $newId = !empty($allIds) ? (max($allIds) + 1) : 1;
-    $pet_info['id'] = $newId;
-    $pets[] = $pet_info;
+    $petInfo['id'] = $newId;
+    $pets[] = $petInfo;
     file_put_contents($filePath, json_encode($pets));
     return $newId;
 }
-// createRecord($filePath, ["", 0, "", "", 0, 0]);
 
 function deleteRecord(string $filePath, int $id): int {
     $pets = getPets($filePath);
@@ -28,16 +27,16 @@ function deleteRecord(string $filePath, int $id): int {
     return $id;
 }
 
-function updateRecord(string $filePath, $pet_info): int {
+function updateRecord(string $filePath, array $petInfo): int {
     $pets = getPets($filePath);
     $idInFile = -1;
-    foreach ($pets as $key => $value) {
-        if ($value['id'] == $pet_info['id']) {
+    foreach ($pets as $key => $pet) {
+        if ($pet['id'] == $petInfo['id']) {
             $idInFile = $key;
-            $pets[$key] = $pet_info;
-            file_put_contents($filePath, json_encode($pets));
+            $pets[$key] = $petInfo;
         }
     }
+    file_put_contents($filePath, json_encode($pets));
     return $idInFile;
 }
 
