330a7104c486dd96ba0407967920ae2becc72f93
gremixas <andrius.agafonovas@gmail.com>
2023-01-30 15:18:35 +0200

    Project continues..

--- PHP/666 Project/handlers/upload.php
+++ PHP/666 Project/handlers/upload.php
@@ -1,10 +1,17 @@
 <?php
 
+if (empty($_POST['id'])) {
+    die("ID nereikia?");
+}
+
 /* Get the name of the file uploaded to Apache */
 $filename = $_FILES['file']['name'];
+$extension = pathinfo($filename)['extension'];
+// die($extension);
 
 /* Prepare to save the file upload to the upload folder */
-$location = __DIR__ . "/../car_images/".$filename;
+// $location = __DIR__ . "/../car_images/" . $filename . ".$extension";
+$location = __DIR__ . "/../car_images/" . $_POST['id'] . ".$extension";
 
 /* Permanently save the file upload to the upload folder */
 if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
@@ -12,5 +19,18 @@ if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) {
 } else { 
     echo '<p>The php and HTML5 file upload failed.</p>'; 
 }
+
+
+// $uploads_dir = '/../car_images';
+
+// foreach ($_FILES["pictures"]["error"] as $key => $error) {
+//     if ($error == UPLOAD_ERR_OK) {
+//         $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+//         // basename() may prevent filesystem traversal attacks;
+//         // further validation/sanitation of the filename may be appropriate
+//         $name = basename($_FILES["pictures"]["name"][$key]);
+//         move_uploaded_file($tmp_name, "$uploads_dir/$name");
+//     }
+// }
 
 ?>
--- PHP/666 Project/index.php
+++ PHP/666 Project/index.php
@@ -35,32 +35,14 @@ $car = new Car("Mazda", "MX5", "2001", "Petrol", "Manual", "156", "car_images/mi
         </div>
     </main>
     <footer id="footer">
-        <!-- <input id="image-file" type="file" />
-        <form enctype="multipart/form-data" action="./index.php" method="post">
-            <input id="image-file" type="file" />
-        </form> -->
-        <!-- <input type="file" name="file" id="file">
-        <button onclick="doupload()" name="submit">Upload File</button> -->
         <form action="handlers/upload.php" method="post" enctype="multipart/form-data" > 
+            <input type="hidden" name="id" value="555">
             <input id="file" type="file" name="file" /> 
-            <!-- <input id="html5-upload-button" type="button" value="Upload" />  -->
             <button type="submit">submit</button>
         </form>
-
     </footer>
     <script>
         const userLoggedIn = <?=$userLoggedIn?>;
-
-        // function doupload() {
-        //     let data = document.getElementById("file").files[0];
-        //     let entry = document.getElementById("file").files[0];
-        //     console.log('doupload',entry,data)
-        //     fetch('./car_images/' + encodeURIComponent(entry.name), {method:'PUT',body:data});
-        //     alert('your file has been uploaded');
-        //     location.reload();
-        // };
-
-
     </script>
 </body>
 </html>
